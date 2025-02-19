0838ccf5b168475fd3510c04bc4ff15298d9a253
Gyubum Kim <kgb2872@gmail.com>
2025-02-19 07:16:31 -0500

    version issue fixed

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ErrorPageController {
 
     //RequestDispatcher 상수로 정의되어 있음
-    public static final String ERROR_EXCEPTION = "javax.servlet.error.exception";
-    public static final String ERROR_EXCEPTION_TYPE = "javax.servlet.error.exception_type";
-    public static final String ERROR_MESSAGE = "javax.servlet.error.message";
-    public static final String ERROR_REQUEST_URI = "javax.servlet.error.request_uri";
-    public static final String ERROR_SERVLET_NAME = "javax.servlet.error.servlet_name";
-    public static final String ERROR_STATUS_CODE = "javax.servlet.error.status_code";
+    public static final String ERROR_EXCEPTION = "jakarta.servlet.error.exception";
+    public static final String ERROR_EXCEPTION_TYPE = "jakarta.servlet.error.exception_type";
+    public static final String ERROR_MESSAGE = "jakarta.servlet.error.message";
+    public static final String ERROR_REQUEST_URI = "jakarta.servlet.error.request_uri";
+    public static final String ERROR_SERVLET_NAME = "jakarta.servlet.error.servlet_name";
+    public static final String ERROR_STATUS_CODE = "jakarta.servlet.error.status_code";
 
     @RequestMapping("/error-page/404")
     public String errorPage404(HttpServletRequest request, HttpServletResponse response) {
@@ -40,5 +40,7 @@ public class ErrorPageController {
         log.info("ERROR_REQUEST_URI: {}", request.getAttribute(ERROR_REQUEST_URI));
         log.info("ERROR_SERVLET_NAME: {}", request.getAttribute(ERROR_SERVLET_NAME));
         log.info("ERROR_STATUS_CODE: {}", request.getAttribute(ERROR_STATUS_CODE));
+        // later
+        log.info("dispatchType={}", request.getDispatcherType());
     }
 }
